bb3f6053d64b9cbd0bcfb7db384a37546cf10fa9
Brian Kemboi <kipkemboikibet1@gmail.com>
2021-07-28 18:42:09 +0300

    changed debug to false and static folder for heroku.

--- kongoni/settings.py
+++ kongoni/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
