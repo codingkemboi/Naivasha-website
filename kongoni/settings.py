c47e027d26f8c84120623ff89addb7a041144cd7
Brian Kemboi <kipkemboikibet1@gmail.com>
2021-07-28 19:41:11 +0300

    Modified statics.

--- kongoni/settings.py
+++ kongoni/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rq3=)hww2ru*!k47pibdnd%!2c8l3t@fzjwpunu(d54h&d-1qx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['kongoni-web.herokuapp.com'] ###### Remove in ( '*' ) production
 
