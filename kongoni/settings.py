96434909d6f6c86c986f0b3aa901f7d0111a395d
Brian Kemboi <kipkemboikibet1@gmail.com>
2021-07-28 17:25:05 +0300

    Modified settings and requirements.

--- kongoni/settings.py
+++ kongoni/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
+from django import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
