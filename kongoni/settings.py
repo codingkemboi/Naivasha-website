55aa71ae7b48acb0b6bddc661c60b7d6ecd5a0d1
Brian Kemboi <kipkemboikibet1@gmail.com>
2021-07-28 21:53:51 +0300

    Modified staticfiles for deploy.

--- kongoni/settings.py
+++ kongoni/settings.py
@@ -149,5 +149,19 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # My login settings
 LOGIN_URL = '/users/login/'
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+# Heroku settings
+if os.getcwd() == '/app':
+    import dj_database_url
+    DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+    }
+    #    Honor the 'X-Forwarded-Proto' header for request.is_secure().
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    # Allow all host headers.
+    ALLOWED_HOSTS = ['*']
+    # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+    )
