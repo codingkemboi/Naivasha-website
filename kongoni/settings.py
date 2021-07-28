51830960fb3b0c1bd57c419ad779c287f205fc27
Brian Kemboi <kipkemboikibet1@gmail.com>
2021-07-28 20:39:40 +0300

    Modified statics.

--- kongoni/settings.py
+++ kongoni/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rq3=)hww2ru*!k47pibdnd%!2c8l3t@fzjwpunu(d54h&d-1qx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['kongoni-web.herokuapp.com'] ###### Remove in ( '*' ) production
+ALLOWED_HOSTS = ['kongoni-web.herokuapp.com', '127.0.0.1'] ###### Remove in ( '*' ) production
 
 
 # Application definition
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
