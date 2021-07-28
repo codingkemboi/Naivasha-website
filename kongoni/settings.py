0c4a6a612929b56010218400957f2d8aa16957da
Brian Kemboi <kipkemboikibet1@gmail.com>
2021-07-28 18:56:09 +0300

    changes to  staticfiles.

--- kongoni/settings.py
+++ kongoni/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
