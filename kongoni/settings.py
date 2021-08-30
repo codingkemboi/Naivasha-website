2ec6da132569c14d2acc3853b1fcb5fd9c6c6c3e
Brian Kemboi <kipkemboikibet1@gmail.com>
2021-08-30 19:41:21 +0300

    serving media images.

--- kongoni/settings.py
+++ kongoni/settings.py
@@ -181,3 +181,6 @@ if os.getcwd() == '/app':
     STATIC_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
